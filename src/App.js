5525bde4e14780ac0b3f64d79fcf7adb70964e83
Fabricio Richieri <fabriciorichieri@yahoo.com>
2022-04-27 02:16:57 -0300

    OK

--- src/App.js
+++ src/App.js
@@ -4,30 +4,38 @@ import React from 'react';
 function App() {
   const [breakLength, setBreakLength] = React.useState(5);
   const [sessionTime, setSessionTime] = React.useState(25);
-  const [clockTime, setClockTime] = React.useState(convertMtoMS(25));
-  const [play, setPlay] = React.useState(false);
+  // const [clockTime, setClockTime] = React.useState(1500);
+  const [play, setPlay] = React.useState(true);
 
-  function convertMtoMS(value) {
-    const minutes = parseInt(value, 10);
-    let seconds = Math.floor(minutes / 60); 
-    if (minutes < 1) {minutes = "0"+minutes;}
-    if (seconds < 1) {seconds = "0"+seconds;}
-    return minutes+':'+seconds; 
+
+  const convertMtoMS = (value) => {
+    let minutes = Math.floor(value / 60);
+    let seconds = value % 60;
+    return (
+      (minutes < 10 ? "0" + minutes : minutes) +
+      ":" +
+      (seconds < 10 ? "0" + seconds : seconds)
+    )
   }
+
+  const [displayClock, setDisplayClock] = React.useState(convertMtoMS(1500));
+
 
   function handleReset() {
     setBreakLength(5)
     setSessionTime(25)
-    setClockTime(25)
+    // setClockTime(25)
+    setDisplayClock(convertMtoMS(1500))
   }
 
   function handleIncrement(e) {
-    if (e.target.id === 'break-increment' && breakLength < 61) {
+    if (e.target.id === 'break-increment' && breakLength < 60) {
       setBreakLength(breakLength + 1);
     }
-    if (e.target.id === 'session-increment') {
+    if (e.target.id === 'session-increment' && sessionTime < 60) {
       setSessionTime(sessionTime + 1);
-      setClockTime(clockTime + 1)
+      // setClockTime(clockTime + 1)
+      setDisplayClock(convertMtoMS(sessionTime * 60 + 60))
     }
   }
 
@@ -37,32 +45,39 @@ function App() {
     }
     if (e.target.id === 'session-decrement' && sessionTime > 1) {
       setSessionTime(sessionTime - 1);
-      setClockTime(clockTime - 1)
+      // setClockTime(clockTime - 1)
+      setDisplayClock(convertMtoMS(sessionTime * 60 - 60))
     }
   }
 
-  function handlePlay() {
-    if (play === false) {
-      setClockTime(pomodoro(sessionTime))
-      setPlay(true)
-    } else {
-      clearInterval(interval)
-      setPlay(false)
-    }
-  }
 
+
+    
     var seconds = 0;
-    var interval ;
+    var interval;
     function pomodoro(mins) {
       seconds = mins*60 || 0;     
-      interval = setInterval(function() {
+      var interval = setInterval(function() {
+            if (play === false) console.log("Debería terminar")
             seconds--;
-            setClockTime(seconds);
+            console.log(convertMtoMS(seconds))
+            setDisplayClock(convertMtoMS(seconds))
             if(!seconds){
                 clearInterval(interval); 
             }
       },1000)
     }
+
+    function handlePlay() {
+      if (play === true) {
+        (pomodoro(sessionTime))
+        setPlay(false)
+      } else {
+        setPlay(true)
+        clearInterval(interval)
+      }
+    }
+
 
   return (
     <div className="AppContainer">
@@ -90,7 +105,7 @@ function App() {
 
       <div className='session' id="timer-label">
         <p>Session</p>
-        <p id="time-left">{clockTime}</p>
+        <p id="time-left">{displayClock}</p>
       </div>
         <button id="start_stop" onClick={handlePlay}>≧</button>
         <button id="reset" onClick={handleReset}>↺</button>
